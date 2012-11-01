e07b0d2dff106cc26f644d8e5c927cc4b7c3281b
João Paulo Seixas Alves e Silva <jpseixasesilva@hotmail.com>
2012-11-01 20:53:40 -0200

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cooperativa::Application.config.secret_token = '3789db07833fa83a646b46572f53a2f9f4020afd8bc4592e82820467e8e8a28f2dbe6629b1a4666eee235c091b8a0685a05677eeef2a92397d767c8f7ef05d76'
